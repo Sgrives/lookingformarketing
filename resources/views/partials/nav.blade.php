b9b4acac8e59bb7537acce46b40edd400b211e01
Jon ODonovan <email@jodonovan.com>
2019-02-23 19:04:39 -0500

    Added logout button

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -38,5 +38,17 @@
 				<a class="nav-link" href="{{url('shop')}}">Shop</a>
 			</li> --}}
 		</ul>
+		<ul class="navbar-nav ml-auto">
+			@guest
+
+			@else
+				<li class="nav-item">
+					<a class="nav-link" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+					<form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
+						{{csrf_field()}}
+					</form>
+				</li>
+			@endif
+		</ul>
 	</div>
 </nav>
