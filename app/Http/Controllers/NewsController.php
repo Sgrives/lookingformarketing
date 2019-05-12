ad6d855b3ce9ee13e5303bc7e4bccd3bbba80b53
Jon ODonovan <email@jodonovan.com>
2019-05-11 21:07:02 -0400

    News feeds update

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -10,27 +10,21 @@ class NewsController extends Controller
     public function index()
     {
         $feed = Feeds::make([
-            //Facebook Newsroom
+            //Official Social Media
             'https://newsroom.fb.com/feed/',
             'https://www.facebook.com/business/news/rss',
+            'https://blog.linkedin.com/topics.rss.html',
+            'https://newsroom.pinterest.com/en/feed/posts.xml',
 
             //Google
-            // 'https://www.blogger.com/feeds/3580069/posts/default',
             'https://www.blogger.com/feeds/32069983/posts/default',
             'https://www.blogger.com/feeds/1090964379634258791/posts/default',
             'https://www.blog.google/products/ads/rss/',
             'https://www.blog.google/products/marketingplatform/360/rss/',
 
-            //LinkedIn
-            'https://blog.linkedin.com/topics.rss.html',
-
-            //Pinterest
-            'https://newsroom.pinterest.com/en/feed/posts.xml',
-
             //Others
             'https://www.marketingweek.com/feed/',
             'http://feeds.searchengineland.com/searchengineland',
-            'http://feeds.seroundtable.com/SearchEngineRoundtable1',
             'https://www.socialmediaexaminer.com/feed/',
             'https://socialmediaexplorer.com/feed/',
             'https://www.socialbakers.com/blog/rss',
@@ -48,12 +42,26 @@ class NewsController extends Controller
             'https://blog.marketo.com/feed',
             'http://feedpress.me/searchenginejournal',
             'https://searchenginewatch.com/feed/',
-            // 'http://www.thesempost.com/feed/',
             'https://yoast.com/seo-blog/feed/',
-
-            //The Verge
             'https://www.theverge.com/rss/facebook/index.xml',
-            'https://www.theverge.com/rss/google/index.xml'
+            'https://www.theverge.com/rss/google/index.xml',
+            'https://www.adweek.com/feed/',
+            'https://www.admonsters.com/feed/',
+            'http://feeds2.feedburner.com/ad-exchange-news',
+            'https://www.adteachings.com/rss',
+            'http://davetrott.co.uk/feed/',
+            'https://www.creativereview.co.uk/feed/',
+            'https://feeds2.feedburner.com/itsnicethat/SlXC',
+            'https://www.fastcompany.com/co-design/rss',
+            'https://www.moderncopywriter.com/feed/',
+            'https://www.marketingweek.com/feed/',
+            'http://feedpress.me/mozblog',
+            'https://blog.hubspot.com/marketing/rss.xml',
+            'https://www.semrush.com/blog/feed/',
+            'https://www.adweek.com/agencyspy/feed/',
+            'http://feeds.feedburner.com/GreatAds',
+            'https://www.adsoftheworld.com/node/feed',
+            'https://mashable.com/category/social-media/feed/'
         ], 3, true);
     
         $data = array(
